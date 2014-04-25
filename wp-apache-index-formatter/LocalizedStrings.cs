974fef38edf45b311495edb6ecde1514872cac7c
Sean Collins <scollins36@gatech.edu>
2014-04-25 12:14:15 -0400

    Initial commit

---
+++ wp-apache-index-formatter/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using wp_apache_index_formatter.Resources;
+
+namespace wp_apache_index_formatter
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
