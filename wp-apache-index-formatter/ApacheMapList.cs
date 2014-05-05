c8e2bdcf7a510361fccc8d2a369f92c7725ba08a
OzuYatamutsu <scollins36@gatech.edu>
2014-05-04 22:15:57 -0400

    Created new dictionary to hold String-Uri responses from server

---
+++ wp-apache-index-formatter/ApacheMapList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp_apache_index_formatter
+{
+    class ApacheMapList<string, string> : Dictionary<string, string>
+    {
+        private Uri endpoint;
+        private HttpWebRequest getUrl;
+        private Stream getStream;
+
+        public ApacheMapList(Uri endpoint)
+        {
+            this.endpoint = endpoint;
+            getUrl = (HttpWebRequest) HttpWebRequest.Create(endpoint);
+            if (!validateEndpoint())
+            {
+                // Throw something?
+            }
+        }
+
+        private bool validateEndpoint()
+        {
+            bool isValid = false;
+            return isValid;
+        }
+    }
+}
